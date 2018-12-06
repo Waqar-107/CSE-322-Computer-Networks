0690192c3f5088350b6341a3e21e4f35f22c13e5
Waqar-107 <waqar.hassan866@gmail.com>
2018-12-07 02:36:23 +0600

    kisui hoynai

--- Offline-2/1505107.cpp
+++ Offline-2/1505107.cpp
@@ -1,9 +1,11 @@
 /*** from dust i have come, dust i will be ***/
 
 #include<bits/stdc++.h>
-#include <arpa/inet.h>
-#include <sys/socket.h>
-#include <unistd.h>
+#include<arpa/inet.h>
+#include<pthread.h>
+#include<semaphore.h>
+#include<sys/socket.h>
+#include<unistd.h>
 
 #define dbg printf("in\n")
 #define nl printf("\n")
@@ -24,8 +26,39 @@ struct route
     }
 };
 
-//key would be the destination
+string myIP;
 map<string,route> routingTable;
+set<string> adj;
+
+int sockfd, bind_flag;
+int bytes_received;
+socklen_t addrlen;
+
+struct sockaddr_in client_address;
+struct sockaddr_in senders_address;
+struct sockaddr_in serve;
+
+void init()
+{
+    client_address.sin_family = AF_INET;
+    client_address.sin_port = htons(4747);
+    inet_pton(AF_INET, myIP.c_str(), &client_address.sin_addr);
+
+    serve.sin_family = AF_INET;
+    serve.sin_port = htons(4747);
+
+    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    bind_flag = bind(sockfd, (struct sockaddr*) &client_address, sizeof(sockaddr_in));
+
+    cout << "\n---------------------------------------\n";
+    if(!bind_flag)
+        cout << "connected!!!\n";
+    else {
+        cout << "connection error\n";
+        exit(0);
+    }
+    cout << "---------------------------------------\n\n";
+}
 
 void showRoutingTable()
 {
@@ -56,25 +89,51 @@ string toString(int n)
     return str;
 }
 
+//------------------------------------------------------------------------
+//thread for sending and updating
+sem_t readyToSend;
+
+void *sendTable(void *arg)
+{
+	char buffer2[1024];
+
+    while(true)
+    {
+        sem_wait(&readyToSend);
+        for(string s : adj)
+        {
+            inet_pton(AF_INET, s.c_str(), &client_address.sin_addr);
+
+            memcpy(&buffer2, &routingTable, sizeof(routingTable));
+            sendto(sockfd, buffer2, 1024, 0, (struct sockaddr*) &serve, sizeof(sockaddr_in));
+        }
+    }
+}
+//------------------------------------------------------------------------
+
 int main(int argc, char *argv[])
 {
     int i, j, k;
     int w;
 
-    string myIP;
+
     string u, v, line;
     vector<int> vec;
+
 
     char *in = argv[1];
     for(i = 0; i< strlen(in); i++)
         myIP.push_back(in[i]);
 
 
+    pthread_t sender;
+    sem_init(&readyToSend, 0, 0);
+    pthread_create(&sender, NULL, sendTable, NULL);
+
+
     //--------------------------------------------------------------------
     //read the file and update the routing table
     ifstream infile(argv[2]);
-    set<string> adj;
-
     while(infile >> u >> v >> w)
     {
         //cout<<u.length()<<" "<<v.length()<<" "<<w<<" "<<myIP.length()<<endl;
@@ -103,28 +162,7 @@ int main(int argc, char *argv[])
 
     //--------------------------------------------------------------------
     //initialize receive mode
-    int sockfd, bind_flag;
-    int bytes_received;
-	socklen_t addrlen;
-
-    struct sockaddr_in client_address;
-    struct sockaddr_in senders_address;
-
-    client_address.sin_family = AF_INET;
-    client_address.sin_port = htons(4747);
-    inet_pton(AF_INET, myIP.c_str(), &client_address.sin_addr);
-
-    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
-    bind_flag = bind(sockfd, (struct sockaddr*) &client_address, sizeof(sockaddr_in));
-
-    cout << "\n---------------------------------------\n";
-    if(!bind_flag)
-        cout << "connected!!!\n";
-    else {
-        cout << "connection error\n";
-        exit(0);
-    }
-    cout << "---------------------------------------\n\n";
+    init();
 
     while(true)
     {
@@ -135,14 +173,18 @@ int main(int argc, char *argv[])
         if(bytes_received != -1)
         {
             string cmd(buffer);
-            cout<<cmd<<endl;
+            //cout<<cmd<<endl;
 
             //show the routing table
             if(cmd.find("show") != string::npos)
                 showRoutingTable();
 
             //exchange routing table
-            else if(cmd.find("clk") != string::npos){}
+            else if(cmd.find("clk") != string::npos)
+            {
+                sem_post(&readyToSend);
+
+            }
 
             else if(cmd.find("cost") != string::npos)
             {
@@ -165,6 +207,52 @@ int main(int argc, char *argv[])
                 else
                     routingTable[u].cost = w;
             }
+
+            else if(cmd.find("send") != string::npos)
+            {
+                vec.clear();
+                for(i = 4; i < bytes_received; i++)
+                {
+                    k = buffer[i];
+                    if(k < 0)
+                        k += 256;
+
+                    vec.push_back(k);
+                }
+
+                u = toString(vec[0]) + "." + toString(vec[1]) + "." + toString(vec[2]) + "." + toString(vec[3]);
+                v = toString(vec[4]) + "." + toString(vec[5]) + "." + toString(vec[6]) + "." + toString(vec[7]);
+                k = vec[8];
+
+                cout<<"u: "<<u<<". v: "<<v<<" mip: "<<myIP<<endl;
+
+                //destination reached
+                string temp = "";
+                if(v == myIP)
+                {
+                    for(i = 9; i <= vec.size(); i++)
+                        temp.push_back((char)vec[i]);
+
+                    cout << u << " sent to " <<v <<" : "<< temp << endl;
+                }
+
+
+
+                else
+                {
+                    //cout<<"u: "<<u<<". v: "<<v<<". msg: "<<temp<<endl;
+                    if(routingTable[v].next_hop != "-")
+                    {
+                        inet_pton(AF_INET, routingTable[v].next_hop.c_str(), &client_address.sin_addr);
+                        sendto(sockfd, buffer, 1024, 0, (struct sockaddr*) &serve, sizeof(sockaddr_in));
+                    }
+
+                    else
+                        cout << "router " << v<< " seems uncreachable at the moment\n";
+                }
+
+                memset(buffer, 0, sizeof(buffer));
+            }
         }
 
         else
@@ -173,5 +261,6 @@ int main(int argc, char *argv[])
 
     //--------------------------------------------------------------------
     //--------------------------------------------------------------------
+
     return 0;
 }
--- Offline-2/rn1505107.sh
+++ Offline-2/rn1505107.sh
@@ -3,6 +3,6 @@
 rname="router"$1".out"
 rname2="./"$rname
 
-g++ 1505107.cpp -o $rname
+g++ 1505107.cpp -lpthread -o $rname
 $rname2 $2 "topo.txt"
 
