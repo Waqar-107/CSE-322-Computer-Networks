9371934d7d92bea7428d9f3bd9bbdb3009772a35
Waqar-107 <waqar.hassan866@gmail.com>
2018-12-22 18:44:58 +0600

    init ns2

---
+++ NS2/basic codes/tcl_commands_basic.tcl
@@ -0,0 +1,35 @@
+#intro to basic commands of tcl - btw this is a comment
+
+#print
+puts "puts command is equivalent to printf"
+
+#set variable
+set x 5
+puts $x
+
+#expression
+puts [expr 1+3]
+set x [expr 107+1]
+puts $x
+puts [expr $x-138]
+
+#conditions
+
+#ternary
+set x 5
+set y 6
+
+set x [expr $x < $y ? 10 : 20]
+puts $x
+
+#normal if - writing in this format is necessary
+if {$x > $y} then {
+    puts "x > y" 
+} else {
+    puts "x < y
+}
+
+#for loop
+for {set i 0} {$i < 10} {set i [expr $i + 1]} {
+    puts $i
+}
