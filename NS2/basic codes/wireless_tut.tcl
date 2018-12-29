9da6482c6d86f71c874438240924f9a4f0a912a1
Waqar-107 <waqar.hassan866@gmail.com>
2018-12-29 18:45:59 +0600

    static aand mobile-wireless 802.11 tcl added

--- NS2/basic codes/wireless_tut.tcl
+++ NS2/basic codes/wireless_tut.tcl
@@ -4,11 +4,11 @@
 # step-3  :  create tracing and animation file
 # step-4  :  topography
 # step-5  :  GOD - General Operations Director
-# step-6  :  create nodes
-# step-7  :  create channel(communication path)
+# step-6  :  create channel(communication path)
+# step-7  :  create nodes
 # step-8  :  position of the nodes (wireless nodes needs a location)
 # step-9  :  any mobilty codes (if nodes are moving)
-# step-10 :  TCP, UDP traffic
+# step-10 :  agent creation - TCP, UDP. Traffic-> ftp, cbr
 
 
 #================================================
@@ -26,8 +26,8 @@ set val(ant)    Antenna/OmniAntenna          ;# antenna model
 set val(ifqlen) 50                           ;# max packet in ifq -50 is optimal
 set val(rp)     AODV                         ;# routing protocol - Adhoc OnDemand Distance Vector
 set val(nn)     6			     ;# node quantity     
-set val(x) 	500			     ;# x dimension
-set val(y) 	500			     ;# y dimension
+set val(x) 	1000			     ;# x dimension
+set val(y) 	1000			     ;# y dimension
 
 # energy parameters - not in the tutorial - taken from raji sir
 set val(energymodel_11)         EnergyModel  ;# it is a c++ class
@@ -73,7 +73,7 @@ create-god $val(nn)	;# create-god "node quantity"
 
 
 #================================================
-# create chanel
+# create channel
 set channel1 [new $val(chan)]
 #================================================
 
@@ -106,14 +106,75 @@ $ns node-config -adhocRouting $val(rp) \
 
 
 #================================================
-# GOD - General Operations Director
+# node creation
+
+# some node declaration
+# $ns node-config -channel $channel2
+# some node declaration
+# in the above way we can make different config for routers
+
+for {set i 0} {$i < $val(nn)} {incr i} {	
+	set node_($i) [$ns node];
+	$node_($i) random-motion 0;#when they move, do not move randomly
+
+}
+
+#set coord of the nodes
+for {set i 0} {$i < $val(nn)} {incr i} {
+	$node_($i) set X_ [expr 100+$i*20]
+	$node_($i) set Y_ [expr 100+100*($i%2)]
+#	$node_($i) set Z_ 0.0
+#}
+
 #================================================
 #================================================
-# GOD - General Operations Director
+# mobility of nodes
+# at what time? which node? where to? at what speed?
+$ns at 1.0 "$node_(1) setdest 490.0 340.0 25.0"
+$ns at 1.0 "$node_(4) setdest 300.0 130.0 5.0"
+$ns at 1.0 "$node_(5) setdest 190.0 440.0 15.0"
+
+#the nodes can move any number of times at any location during simulation
 #================================================
 #================================================
-# GOD - General Operations Director
+# create agents
+set tcp [new Agent/TCP]
+set sink [new Agent/TCPSink]
+
+$ns attach-agent $node_(0) $tcp
+$ns attach-agent $node_(5) $sink
+
+set ftp [new Application/FTP]
+$ftp attach-agent $tcp
+
+$ns at 1.0 "$ftp start"
+
+
+set udp [new Agent/UDP]
+set null [new Agent/Null]
+
+$ns attach-agent $node_(2) $udp
+$ns attach-agent $node_(3) $null
+
+set cbr [new Application/Traffic/CBR]
+$cbr attach-agent $udp
+$ns at 1.0 "$cbr start"
 #================================================
+
 #================================================
-# GOD - General Operations Director
+# finish procedure
+proc finish {} {
+	puts "finishing"
+	global ns tracefd namtrace
+	$ns flush-trace
+	close $tracefile
+	close $namfile
+	exec nam wireless_tut.nam &
+        exit 0
+}
+
+$ns at 10.0 "finish"
 #================================================
+
+puts "Starting Simulation"
+$ns run
