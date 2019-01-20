ec2bb0fc15910d35f5e89fd40474feafc4d2ae35
Waqar-107 <waqar.hassan866@gmail.com>
2019-01-20 19:18:41 +0600

    init - tcp

---
+++ Offline-4/TCP/cmake-build-debug/CMakeFiles/TCP.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TCP.dir/rdt_abp.c.obj"
+  "TCP.pdb"
+  "TCP.exe"
+  "TCP.exe.manifest"
+  "libTCP.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/TCP.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
